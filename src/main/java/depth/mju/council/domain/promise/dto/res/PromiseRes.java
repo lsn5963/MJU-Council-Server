0604d7b960be075100cf1115607b4ba9f37fda80
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2024-11-27 23:02:30 +0900

    fix/integer->Long, string->String 오타 수정

--- src/main/java/depth/mju/council/domain/promise/dto/res/PromiseRes.java
+++ src/main/java/depth/mju/council/domain/promise/dto/res/PromiseRes.java
@@ -9,14 +9,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class PromiseRes {
-    @Schema(type = "integer", example = "1", description = "공약 ID")
+    @Schema(type = "Long", example = "1", description = "공약 ID")
     private Long id;
-    @Schema(type = "string", example = "재수강 학점 A0 확대", description = "공약 제목")
+    @Schema(type = "String", example = "재수강 학점 A0 확대", description = "공약 제목")
     private String title;
 
-    @Schema(type = "string", example = "재수강 시 취득 가능한 학점을 A0로 확대하겠습니다", description = "공약 내용")
+    @Schema(type = "String", example = "재수강 시 취득 가능한 학점을 A0로 확대하겠습니다", description = "공약 내용")
     private String content;
 
-    @Schema(type = "integer", example = "0", description = "공약 진행 정도 (0: 진행x, 1: 진행중, 2: 진행완료)")
+    @Schema(type = "Long", example = "0", description = "공약 진행 정도 (0: 진행x, 1: 진행중, 2: 진행완료)")
     private Long progress;
 }
