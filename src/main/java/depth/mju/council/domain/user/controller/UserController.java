305c54d49258e856fb500929e926eee54ba1d7a2
suyeon <ibaesuyeon@naver.com>
2024-11-28 15:48:22 +0900

    [DOCS] 회원가입, 로그인 스웨거 명세 추가

--- src/main/java/depth/mju/council/domain/user/controller/UserController.java
+++ src/main/java/depth/mju/council/domain/user/controller/UserController.java
@@ -4,6 +4,8 @@ import depth.mju.council.domain.user.dto.req.LoginReq;
 import depth.mju.council.domain.user.dto.req.RegisterReq;
 import depth.mju.council.domain.user.dto.res.JWTAuthResponse;
 import depth.mju.council.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,14 +23,18 @@ public class UserController {
         return "It's Working in User Service";
     }
 
+    @Operation(summary = "로그인")
     @PostMapping("/login")
     public ResponseEntity<JWTAuthResponse> login(
+            @Parameter(description = "Schemas의 LoginReq를 참고해주세요.", required = true) @RequestBody @Valid LoginReq loginReq) {
         JWTAuthResponse token = userService.login(loginReq);
         return ResponseEntity.ok(token);
     }
 
+    @Operation(summary = "회원가입")
     @PostMapping("/register")
     public ResponseEntity<String> register(
+            @Parameter(description = "Schemas의 RegisterReq를 참고해주세요.", required = true) @RequestBody @Valid RegisterReq registerReq) {
         String response = userService.register(registerReq);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
--- src/main/java/depth/mju/council/domain/user/dto/req/RegisterReq.java
+++ src/main/java/depth/mju/council/domain/user/dto/req/RegisterReq.java
@@ -1,5 +1,6 @@
 package depth.mju.council.domain.user.dto.req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -14,10 +15,12 @@ public class RegisterReq {
 
     @NotNull(message = "ID(username)을 입력해주세요")
     @Size(min = 5, message = "ID(username)은 5자 이상이어야 합니다")
+    @Schema(type = "String", example = "si14444", description ="로그인 ID")
     private String username;
 
     @NotNull(message = "비밀번호를 입력해주세요")
     @Size(min = 5, message = "비밀번호는 5자 이상이어야 합니다")
+    @Schema(type = "String", example = "pw44441", description ="로그인 비밀번호")
     private String password;
 
 }
