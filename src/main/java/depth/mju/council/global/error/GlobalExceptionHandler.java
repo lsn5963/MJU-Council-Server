c76b23afbd3729faa41711828b7448c2096afc19
lsn5963 <77337977+lsn5963@users.noreply.github.com>
2024-11-29 01:35:56 +0900

    fix/파일이름변경

--- src/main/java/depth/mju/council/global/error/GlobalExceptionHandler.java
+++ src/main/java/depth/mju/council/global/error/GlobalExceptionHandler.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(annotations = RestController.class)
-public class ApiControllerAdvice {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected ResponseEntity<?> handleHttpRequestMethodNotSupportedException(
