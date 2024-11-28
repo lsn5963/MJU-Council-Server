9320a1d449e29ac06fd688e080aa7f7a7f9237cb
suyeon <ibaesuyeon@naver.com>
2024-11-29 01:51:26 +0900

    [FEAT] 레포지토리, 트랜잭션 어노테이션 추가
    Repository에 @Repository추가
    Service에 @Transactional 추가

--- src/main/java/depth/mju/council/domain/user/repository/UserRepository.java
+++ src/main/java/depth/mju/council/domain/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package depth.mju.council.domain.user.repository;
 
 import depth.mju.council.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
--- src/main/java/depth/mju/council/domain/user/service/UserService.java
+++ src/main/java/depth/mju/council/domain/user/service/UserService.java
@@ -15,9 +15,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
