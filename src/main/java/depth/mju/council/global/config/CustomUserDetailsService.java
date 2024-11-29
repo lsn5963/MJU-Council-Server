9d7ca61d0740fc6ac1cde952328e57d2b2f2ad58
suyeon <ibaesuyeon@naver.com>
2024-11-29 20:44:14 +0900

    [FIX] token에서 username 받아오는 메소드 수정
    CustomUserDetailsService에 null 검증 조건문도 함께 추가

--- src/main/java/depth/mju/council/global/config/CustomUserDetailsService.java
+++ src/main/java/depth/mju/council/global/config/CustomUserDetailsService.java
@@ -2,7 +2,6 @@ package depth.mju.council.global.config;
 
 import depth.mju.council.domain.user.entity.UserEntity;
 import depth.mju.council.domain.user.repository.UserRepository;
-import depth.mju.council.global.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,10 +18,11 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (username == null || username.trim().isEmpty()) {
+            throw new UsernameNotFoundException("사용자 이름이 잘못되었습니다.");
+        }
         UserEntity userEntity = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 회원입니다"));
-
-        // UserPrincipal을 반환
         return UserPrincipal.from(userEntity);
     }
 
