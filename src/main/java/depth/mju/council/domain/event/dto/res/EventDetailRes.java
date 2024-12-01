bf8b81df4c806d0c75b8a7d87798c921793389fb
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2024-12-02 00:53:46 +0900

    [DOCS] 스웨거 명세 수정

--- src/main/java/depth/mju/council/domain/event/dto/res/EventDetailRes.java
+++ src/main/java/depth/mju/council/domain/event/dto/res/EventDetailRes.java
@@ -14,7 +14,7 @@ public class EventDetailRes {
     @Schema(type = "String", example = "2024년 명지대학교 축제 세부사항01", description = "행사 세부사항의 제목")
     public String title;
 
-    @Schema(type = "String", example = "2024년 명지대학교 축제를 실시합니다.", description = "행사의 내용")
+    @Schema(type = "String", example = "2024년 명지대학교 축제를 실시합니다.", description = "행사 세부사항의 내용")
     public String content;
 
     @Schema(type = "LocalDate", example = "2024-11-17", description = "행사 세부사항의 작성일자")
