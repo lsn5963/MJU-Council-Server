ffe654674e7e88da8e393dfb8ae8b17ecd6a62cc
suyeon <ibaesuyeon@naver.com>
2024-12-03 09:09:37 +0900

    [FEAT] 중운위(단과대) 조회

--- src/main/java/depth/mju/council/domain/committe/controller/CommitteeController.java
+++ src/main/java/depth/mju/council/domain/committe/controller/CommitteeController.java
@@ -1,13 +1,26 @@
 package depth.mju.council.domain.committe.controller;
 
 import depth.mju.council.domain.committe.service.CommitteeService;
+import depth.mju.council.global.payload.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/committees")
+@RequestMapping("/api/v1/committees")
 @RequiredArgsConstructor
 public class CommitteeController {
     private final CommitteeService committeeService;
+
+    @Operation(summary = "중운위(단과대) 조회")
+    @GetMapping("")
+    public ResponseEntity<ApiResult> getAllCommittees() {
+        ApiResult apiResult = ApiResult.builder()
+                .check(true)
+                .information(committeeService.getAllCommittees())
+                .message("중운위(단과대)를 성공적으로 조회했습니다.")
+                .build();
+        return ResponseEntity.ok(apiResult);
+    }
 }
--- src/main/java/depth/mju/council/domain/committe/service/CommitteeService.java
+++ src/main/java/depth/mju/council/domain/committe/service/CommitteeService.java
@@ -1,9 +1,33 @@
 package depth.mju.council.domain.committe.service;
 
+import depth.mju.council.domain.committe.dto.res.CommitteeRes;
+import depth.mju.council.domain.committe.entity.Committee;
+import depth.mju.council.domain.committe.repository.CommitteeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CommitteeService {
+    private final CommitteeRepository committeeRepository;
+
+    @Transactional(readOnly = true)
+    public List<CommitteeRes> getAllCommittees() {
+        List<Committee> committees = committeeRepository.findAll();
+
+        return committees.stream()
+                .map(committee -> CommitteeRes.builder()
+                        .committeeId(committee.getId())
+                        .description(committee.getDescription())
+                        .college(committee.getCollege())
+                        .name(committee.getName())
+                        .pageUrl(committee.getPageUrl())
+                        .snsUrl(committee.getSnsUrl())
+                        .imgUrl(committee.getImgUrl())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
