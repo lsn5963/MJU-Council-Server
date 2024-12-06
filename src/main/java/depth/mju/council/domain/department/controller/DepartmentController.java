1b6a39045a15d7a4f107c218d74714d32afc11f9
suyeon <ibaesuyeon@naver.com>
2024-12-07 04:59:05 +0900

    [FEAT] 국별 소개 조회

--- src/main/java/depth/mju/council/domain/department/controller/DepartmentController.java
+++ src/main/java/depth/mju/council/domain/department/controller/DepartmentController.java
@@ -1,13 +1,29 @@
 package depth.mju.council.domain.department.controller;
 
 import depth.mju.council.domain.department.service.DepartmentService;
+import depth.mju.council.global.config.UserPrincipal;
+import depth.mju.council.global.payload.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/api/departments")
+@RequestMapping("/api/v1/departments")
 @RequiredArgsConstructor
 public class DepartmentController {
     private final DepartmentService departmentService;
-}
+
+    @Operation(summary = "국별 소개 조회")
+    @GetMapping("")
+    public ResponseEntity<ApiResult> getAllDepartments() {
+        ApiResult apiResult = ApiResult.builder()
+                .check(true)
+                .information(departmentService.getAllDepartments())
+                .message("국별 소개를 성공적으로 조회했습니다.")
+                .build();
+        return ResponseEntity.ok(apiResult);
+    }
+
--- src/main/java/depth/mju/council/domain/department/service/DepartmentService.java
+++ src/main/java/depth/mju/council/domain/department/service/DepartmentService.java
@@ -1,9 +1,40 @@
 package depth.mju.council.domain.department.service;
 
+import depth.mju.council.domain.department.dto.res.DepartmentRes;
+import depth.mju.council.domain.department.entity.Department;
+import depth.mju.council.domain.department.repository.DepartmentRepository;
+import depth.mju.council.domain.user.entity.UserEntity;
+import depth.mju.council.domain.user.repository.UserRepository;
+import depth.mju.council.global.config.UserPrincipal;
+import depth.mju.council.global.error.DefaultException;
+import depth.mju.council.global.payload.ErrorCode;
+import depth.mju.council.infrastructure.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class DepartmentService {
+    private final DepartmentRepository departmentRepository;
+    private final UserRepository userRepository;
+    private final S3Service s3Service;
+
+    @Transactional(readOnly = true)
+    public List<DepartmentRes> getAllDepartments() {
+        List<Department> departments = departmentRepository.findAll();
+
+        return departments.stream()
+                .map(department -> DepartmentRes.builder()
+                        .departmentId(department.getId())
+                        .description(department.getDescription())
+                        .imgUrl(department.getImgUrl())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
 }
